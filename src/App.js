e769efbac285a037a9c65e3f73b9d8b1b8763ce8
Maksat <makaevmaksat2001@gmail.com>
2022-09-01 15:56:07 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/style.scss'
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Hero from "./components/hero/hero";
+import Advan from "./components/advan/advan";
+import Services from "./components/services/services";
+import Customer from "./components/customer/customer";
+import Contact from "./components/contact/contact";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <Hero/>
+            <Advan/>
+            <Services/>
+            <Customer/>
+            <Contact/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
